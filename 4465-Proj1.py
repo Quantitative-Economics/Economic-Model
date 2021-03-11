7cc5ced706ee9ca04ab97c21376571284ee9f2be
Christopher <68748006+The-Boffin@users.noreply.github.com>
2021-03-11 06:20:33 -0500

    Update 4465-Proj1.py

--- 4465-Proj1.py
+++ 4465-Proj1.py
@@ -1,31 +1,84 @@
 import random
 
+
+numPeople = 10
+NumBuyers = int(.5 * numPeople)
+NumSellers = int(.5 * numPeople)
+
+M = 1000000
+V = 2.5
+P = 2.5
+Q = 1000000
+
+assert M * V == P * Q, "False equality."
+
+
+
 class Seller:
-    def __init__(self, money, pref, min):
-        self.money = money
-        self.pref = pref
-        self.min = min
+
+    income = None
+
+    pref = None
+    min = None
+
+    def __init__(self, income):
+        self.income = income
+        self.prices()
+        
+
+    def prices(self):
+        self.pref = random.randint(20,60)
+        self.min = self.pref - 10
     
+
 
 class Buyer:
-    def __init__(self, money, pref, max):
-        self.money = money
-        self.pref = pref
-        self.max = max
 
-    
-numPeople = 10
+    income = None
 
-money = [random.randint(40000,100000) for x in range(numPeople)]
+    pref = None
+    max = None
 
-buyerPref = [random.randint(0,40) for x in range(numPeople/2)]
-buyerMax = [x+10 for x in buyerPref]
+    def __init__(self, income):
+        self.income = income
+        self.prices()
+        
 
-sellerPref = [random.randint(20,60) for x in range(numPeople/2)]
-sellerMin = [x-10 for x in sellerPref]
+    def prices(self):
+        self.pref = random.randint(0,40)
+        self.max = self.pref + 10
+
+    def negotiateWith(self, seller):
+
+        if (seller.pref <= self.pref):
+            print("Easily Sold")
+            return
+
+        if (seller.min > self.max):
+            print("Not Sold")
+            return
+
+        print("Sold after Negotiating")
 
 
-for i in range(numPeople):
-    Buyer(money[i], buyerPref[i], buyerMax[i])
-    Seller(money[i], sellerPref[i], sellerMin[i])
-    
+#Needs to add up to total money supply
+incomes = [random.randint(30000,120000) for x in range(numPeople)]
+
+# Other Info that we will pass to the agents
+
+
+
+Buyers = []
+Sellers = []
+
+for i in range(NumBuyers):
+    Buyers.append(Buyer(incomes[i]))
+
+for i in range(NumSellers):
+    Sellers.append(Seller(incomes[i+NumBuyers]))
+
+
+
+for i in range(NumBuyers):
+    for j in range(NumSellers):
+        Buyers[i].negotiateWith(Sellers[j])
